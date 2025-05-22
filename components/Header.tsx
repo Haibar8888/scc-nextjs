3cf96fc1fb82fa0ec45a20a494882278acc19320
Haibar <46429834+Haibar8888@users.noreply.github.com>
2025-05-22 08:55:49 +0800

    dashboard sidebar dan content done

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,27 +1,29 @@
-'use client';
-import { usePathname } from 'next/navigation';
+"use client";
+import { usePathname } from "next/navigation";
+import Image from "next/image";
+
 const getTitle = (path: string) => {
-	if (path.startsWith('/dashboard/users')) return 'Users';
-	if (path.startsWith('/dashboard/settings')) return 'Settings';
-	if (path.startsWith('/dashboard')) return 'Dashboard';
-	if (path.startsWith('/emergency')) return 'Emergency';
-	return 'Page';
+  if (path.startsWith("/dashboard/users")) return "Users";
+  if (path.startsWith("/dashboard/settings")) return "Settings";
+  if (path.startsWith("/dashboard")) return "Dashboard";
+  if (path.startsWith("/emergency")) return "Emergency";
+  return "Page";
 };
 
 const Header = () => {
-	const pathname = usePathname();
-	const title = getTitle(pathname);
+  const pathname = usePathname();
+  const title = getTitle(pathname);
 
-	return (
-		<div className="bg-[#FFFFFF] mb-[20px] text-[#1E1E1E] px-[40px] py-[21px] w-full h-[80px] flex flex-row justify-between items-center border-b border-gray-100">
-			<h1>{title}</h1>
-			<div className="flex gap-2">
-				<p>profile</p>
-				<p>about</p>
-				<p>logout</p>
-			</div>
-		</div>
-	);
+  return (
+    <div className="bg-[#FFFFFF] mb-[20px] text-[#1E1E1E] px-[40px] py-[21px] w-full h-[80px] flex flex-row justify-between items-center border-b border-gray-100">
+      <h1>{title}</h1>
+      <div className="flex gap-[14px] items-center">
+        <Image src="/assets/messages.png" alt="" width={24} height={24} />
+        <Image src="/assets/notification.svg" alt="" width={24} height={24} />
+        <Image src="/assets/profile.png" alt="" width={30} height={30} />
+      </div>
+    </div>
+  );
 };
 
 export default Header;
--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,56 +1,62 @@
 // components/Sidebar.tsx
-'use client';
+"use client";
 
-import Link from 'next/link';
-import { usePathname } from 'next/navigation';
-import clsx from 'clsx'; // opsional, buat bantu toggle class
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import clsx from "clsx";
+import Image from "next/image"; // opsional, buat bantu toggle class
+import { LayoutDashboard, Ambulance, Settings, UserPen } from "lucide-react";
 
 const Sidebar = () => {
-	const pathname = usePathname();
+  const pathname = usePathname();
 
-	return (
-		<aside className="w-64 h-screen bg-[#FFFFFF] text-[#252F4A] p-8 border-r border-gray-100">
-			<h2 className="text-xl font-bold mb-6">Admin Panel</h2>
-			<nav className="flex flex-col space-y-2">
-				<Link
-					href="/dashboard"
-					className={clsx(
-						'p-2 rounded hover:text-[#1B84FF]',
-						pathname === '/dashboard' && 'text-[#1B84FF]'
-					)}
-				>
-					Dashboard
-				</Link>
-				<Link
-					href="/emergency"
-					className={clsx(
-						'p-2 rounded hover:text-[#1B84FF]',
-						pathname === '/emergency' && 'text-[#1B84FF]'
-					)}
-				>
-					Emergency
-				</Link>
-				<Link
-					href="/dashboard/users"
-					className={clsx(
-						'p-2 rounded hover:text-[#1B84FF]',
-						pathname === '/dashboard/users' && 'text-[#1B84FF]'
-					)}
-				>
-					Users
-				</Link>
-				<Link
-					href="/dashboard/settings"
-					className={clsx(
-						'p-2 rounded hover:text-[#1B84FF]',
-						pathname === '/dashboard/settings' && 'text-[#1B84FF]'
-					)}
-				>
-					Settings
-				</Link>
-			</nav>
-		</aside>
-	);
+  return (
+    <aside className="w-64 h-screen bg-[#FFFFFF] text-[#252F4A] p-8 border-r border-gray-100">
+      <Image src="/assets/logo.png" alt="" width={135} height={22} />
+      <nav className="flex flex-col space-y-2 mt-[20px]">
+        <Link
+          href="/dashboard"
+          className={clsx(
+            "flex items-center gap-[10px] p-2 rounded hover:text-[#1B84FF]",
+            pathname === "/dashboard" && "text-[#1B84FF]"
+          )}
+        >
+          <LayoutDashboard size={20} />
+          Dashboard
+        </Link>
+        <Link
+          href="/emergency"
+          className={clsx(
+            "flex items-center gap-[10px] p-2 rounded hover:text-[#1B84FF]",
+            pathname === "/emergency" && "text-[#1B84FF]"
+          )}
+        >
+          <Ambulance size={20} />
+          Emergency
+        </Link>
+        <Link
+          href="/dashboard/users"
+          className={clsx(
+            "flex items-center gap-[10px] p-2 rounded hover:text-[#1B84FF]",
+            pathname === "/dashboard/users" && "text-[#1B84FF]"
+          )}
+        >
+          <UserPen size={20} />
+          Users
+        </Link>
+        <Link
+          href="/dashboard/settings"
+          className={clsx(
+            "flex items-center gap-[10px] p-2 rounded hover:text-[#1B84FF]",
+            pathname === "/dashboard/settings" && "text-[#1B84FF]"
+          )}
+        >
+          <Settings size={20} />
+          Settings
+        </Link>
+      </nav>
+    </aside>
+  );
 };
 
 export default Sidebar;
